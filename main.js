0bfef52977d75f9db70845c5c760ea995e9e3e14
Ngomeli <joshuangomeli@gmail.com>
2020-12-31 03:51:07 +0300

    added the product detail on the product detail html file

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ function menutoggle() {
         MenuItems.style.maxHeight = "0px";
     }
 }
+
+// Js for Product Detail
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("smallImg");
+smallImg[0].onclick = function() {
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function() {
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function() {
+    productImg.src = smallImg[3].src;
+}
